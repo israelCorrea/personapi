fbbc48bea0785287e6a3656e9e264ef69f966d41
israelCorrea <emerick.correa@gmail.com>
2021-10-18 19:09:31 -0300

    Primeira entrega do projeto.

---
+++ src/main/java/com/digitalinnovationone/personapi/SpringWebMvc1Application.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvc1Application.class, args);
+	}
+
+}
